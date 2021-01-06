94ade1bf21faa37753e0f2e7a26e9474e55325bf
morgancostigan <morgan.costigan@gmail.com>
2021-01-06 13:54:25 -0600

    built 404 page

---
+++ gatsby/.cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/macbookpro/Documents/Tier-4/master-gatsby-master/starter-files/gatsby/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/macbookpro/Documents/Tier-4/master-gatsby-master/starter-files/gatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-404-js": () => import("/Users/macbookpro/Documents/Tier-4/master-gatsby-master/starter-files/gatsby/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
+}
+
---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+    return <p>Hmmm, that page doesn't seem to exist.  Probably your fault.</p>
+    
+}
