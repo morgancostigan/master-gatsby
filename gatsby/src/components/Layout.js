6281b94763094d567c455a1c5f99f0f1929d0575
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 14:47:03 -0600

    built Typography.js and imported into Layout.js

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -3,11 +3,13 @@ import Footer from './Footer';
 import Nav from './Nav';
 import 'normalize.css'; //brings all browsers to same baseline css, before styling
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout(props) {
     return (
         <div>
             <GlobalStyles/>
+            <Typography/>
             <Nav />
             {props.children}
             <Footer />
