0e9ba57556842bf8ec3f5052dc29003c55d3a63e
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 14:43:09 -0600

    imported GlobalStyles into Layout.js

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
 import 'normalize.css'; //brings all browsers to same baseline css, before styling
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout(props) {
     return (
         <div>
+            <GlobalStyles/>
             <Nav />
             {props.children}
             <Footer />
