af174a9c921ef0b5311adabd3cc8e6b0a6a75fb7
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 14:32:48 -0600

    imported normalize.css in Layout.js to bring css to uniform baseline, built GlobalStyles.js to use styles in Styled Components

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css'; //brings all browsers to same baseline css, before styling
 
 export default function Layout(props) {
     return (
