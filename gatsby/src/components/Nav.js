e90a61b75440aaacd552f09389ddd283a6ba1af2
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 15:04:48 -0600

    imported styled-components in Nav.js, built NavStyles component, defined styles for nav and ul

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import { Link } from 'gatsby';
 //in Gatsby Link to is faster than href
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+    margin-bottom: 3rem;
+    ul {
+        margin: 0;
+        padding: 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr auto 1fr 1fr;
+        grid-gap: 2rem;
+        align-items: center; 
+        text-align: center;
+        list-style: none;
+    }
+`;
 
 
 export default function Nav() {
-    return <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/pizzas">Pizza Menu</Link>
-            </li>
-            <li>
-                <Link to="/">LOGO</Link>
-            </li>
-            <li>
-                <Link to="/slicemasters">Slice Masters</Link>
-            </li>
-            <li>
-                <Link to="/order">Order Ahead!</Link>
-            </li>
-        </ul>
-    </nav>
-}
+    return (
+        <NavStyles>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/pizzas">Pizza Menu</Link>
+                </li>
+                <li>
+                    <Link to="/">LOGO</Link>
+                </li>
+                <li>
+                    <Link to="/slicemasters">Slice Masters</Link>
+                </li>
+                <li>
+                    <Link to="/order">Order Ahead!</Link>
+                </li>
+            </ul>
+        </NavStyles>
+    )
+}  
