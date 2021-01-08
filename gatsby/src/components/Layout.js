3b77350aa4fcbebde5e868bb3b441f4f2686f9d5
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 15:39:49 -0600

    built ContentStyles and SiteBorderStyles in Layout.js, wrapped the rest of the content in ContentStyles nested in SiteBorderStyles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -4,15 +4,30 @@ import Nav from './Nav';
 import 'normalize.css'; //brings all browsers to same baseline css, before styling
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import styled from 'styled-components';
+
+const SiteBorderStyles = styled.div`
+    max-width: 1000px;
+    margin: 12rem auto 4rem auto;
+`;
+
+const ContentStyles = styled.div`
+    background: white;
+    padding: 2rem;
+`;
 
 export default function Layout(props) {
     return (
-        <div>
+        <>
             <GlobalStyles/>
             <Typography/>
-            <Nav />
-            {props.children}
-            <Footer />
-        </div>
+            <SiteBorderStyles>
+                <ContentStyles>
+                    <Nav />
+                    {props.children}
+                    <Footer />
+                </ContentStyles>
+            </SiteBorderStyles>
+        </>
     );    
 }
