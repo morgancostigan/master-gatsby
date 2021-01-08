80fb536af2b80cd545b0916c0ee702651cda4692
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 15:12:31 -0600

    added rotate styles to li items in Nav.js

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -15,6 +15,23 @@ const NavStyles = styled.nav`
         text-align: center;
         list-style: none;
     }
+    li {
+        --rotate: -2deg;
+        transform: rotate(var(--rotate));
+        order: 1;
+        &:nth-child(1) {
+            --rotate: 1deg;
+        }
+        &:nth-child(2) {
+            --rotate: -2.5deg;
+        }
+        &:nth-child(4) {
+            --rotate: 2.5deg;
+        }
+        &:nth-child(1) {
+            --rotate: -1deg;
+        }
+    }
 `;
 
 
