516c483f7ad8623ceec67824a2cdfeb6ad2e9cdb
morgancostigan <morgan.costigan@gmail.com>
2021-01-08 15:54:57 -0600

    imported stripes into Layout.js, built out SireBorderStyles including clamping top margin and the padding and adding the stripes to the background

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -5,11 +5,16 @@ import 'normalize.css'; //brings all browsers to same baseline css, before styli
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 import styled from 'styled-components';
+import stripes from '../assets/images/stripes.svg';
 
 const SiteBorderStyles = styled.div`
     max-width: 1000px;
     margin: 12rem auto 4rem auto;
-    margin-top: clamp(2rem, 10vw, 12rem);
+    margin-top: clamp(2rem, 10vw, 12rem); //clamped here rather than on margin, as some browsers don't yet support clamp
+    background: white url(${stripes});
+    padding: 5px;
+    padding: clamp(5px, 1vw, 25px);
+    box-shadow: 0, 0, 5px, 3px rgba(0, 0, 0, 0.44)
 `;
 
 const ContentStyles = styled.div`
