6e577114df8a2fdb47646b8522f072fa868d27a3
morgancostigan <morgan.costigan@gmail.com>
2021-01-12 22:29:57 -0600

    added a components folder and started PriceInput.js component

---
+++ sanity/components/PriceInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PriceInput() {
+    return (
+        <div>
+            <h2>How Many Dollars!?!?!</h2>
+        </div>
+    );
+}//end PriceInput function
