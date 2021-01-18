c5eb0ab2bd1b59882f33372f64fd2dfad4e58286
morgancostigan <morgan.costigan@gmail.com>
2021-01-18 14:24:58 -0600

    added more styles to PizzaList

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -6,22 +6,37 @@ import styled from 'styled-components';
 const PizzaGridStyles = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 4rem;
+    grid-auto-rows: auto auto 500px; //this is title row, toppings row, image row
+`;
 
+const PizzaStyles = styled.div`
+    display: grid;
+    @supports not (grid-template-rows: subgrid){
+        grid-template-rows: auto auto 1fr; //for chrome, as it doesn't support subgrid
+    };
+    grid-template-rows: subgrid; //takes row sizing from PizzaGridStyles
+    grid-row: span 3;
+    gap: 1rem;
+    h2, 
+    p {
+        margin: 0;
+    };
 `;
 
 function SinglePizza({pizza}) {
-    return <div>
+    return <PizzaStyles>
         <Link to={`/pizza/${pizza.slug.current}`}>
             <h2>
                 <span className="mark">{pizza.name}</span>
             </h2>
+        </Link>
             <p>
                 {pizza.toppings.map(topping => topping.name).join(', ')}
             </p>
             <Img fluid={pizza.image.asset.fluid} alt={pizza.name}/>
-        </Link>
-    </div>
-}
+    </PizzaStyles>
+};
 
 export default function PizzaList({pizzas}) {
     return <PizzaGridStyles>
