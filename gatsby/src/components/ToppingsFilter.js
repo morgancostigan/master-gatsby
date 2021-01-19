36684a633f958bf784be49aa4d6ab0547c58c402
morgancostigan <morgan.costigan@gmail.com>
2021-01-19 12:53:01 -0600

    counts are now showing accurately in array

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -4,10 +4,35 @@ import React from 'react';
 function countPizzasForTopping(pizzas) {
     //return the toppings with counts
     //flat() turns an array of arrays into one large array
-    return pizzas.map((pizza) => pizza.toppings).flat(); 
-    
-    
-}
+    //reduce take arguments accumulator and the topping
+    const counts = pizzas.map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+        //1. check if topping exists
+        const existingTopping = acc[topping.id];
+        //2. if yes, increment 
+        if (existingTopping) {
+            console.log('ex top,', existingTopping.name);
+            
+            existingTopping.count += 1
+        }
+        //3. if no, create new entry in accumulator
+        else {
+            console.log('new top,', topping.name);
+            acc[topping.id] = {
+                id: topping.id,
+                name: topping.name,
+                count: 1,
+            }
+        };
+
+        return acc;
+    }, {}); 
+    // sort by toping popularity
+    const sortedToppings = Object.values(counts)
+        .sort((a, b) => b.count - a.count);
+    return sortedToppings;
+}// end countPizzasForTopping
 
 export default function ToppingsFilter() {
     //1. get a list of all toppings
