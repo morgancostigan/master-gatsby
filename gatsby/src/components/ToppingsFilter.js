60b0a765825d1902142b27f6722dddfb1a8d2130
morgancostigan <morgan.costigan@gmail.com>
2021-01-19 12:30:01 -0600

    use flat to make one master array of all the topings on all the pizzas

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -1,12 +1,44 @@
- import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
 
- export default function ToppingsFilter() {
+function countPizzasForTopping(pizzas) {
+    //return the toppings with counts
+    //flat() turns an array of arrays into one large array
+    return pizzas.map((pizza) => pizza.toppings).flat(); 
+    
+    
+}
+
+export default function ToppingsFilter() {
     //1. get a list of all toppings
     //2. get a list of all pizzas with their toppings
+    const { toppings, pizzas } = useStaticQuery(graphql`
+        query {
+            toppings: allSanityTopping {
+                nodes {
+                    id
+                    name
+                    vegetarian
+                }
+            }
+            pizzas: allSanityPizza {
+                nodes {
+                    toppings {
+                        name
+                        id
+                    }
+                }
+            }
+        }
+    `)
+    console.clear();
     //3. count how many pizzas have eacch topping
+    const toppingsWithCounts = countPizzasForTopping(pizzas.nodes);
+    console.log({toppingsWithCounts});
+    
     //4. loop over topping list and display topping name and pizza count for each
     //5. Link it
     return <div>
         <p>Toppins</p>
     </div>
- }
+}
