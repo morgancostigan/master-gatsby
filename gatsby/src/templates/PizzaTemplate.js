c49c8493c3a5cb31be275d278fe3fd17932bb9a1
morgancostigan <morgan.costigan@gmail.com>
2021-01-19 14:35:01 -0600

    PizzaTemplate now displaying img, name and toppings on dom

--- gatsby/src/templates/PizzaTemplate.js
+++ gatsby/src/templates/PizzaTemplate.js
@@ -1,8 +1,20 @@
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
 
-export default function SinglePizzaPage() {
-    return <p>single pizza</p>
+export default function SinglePizzaPage({data: {pizza}}) {
+    // console.log({pizza});
+    return <div>
+        <Img fluid={pizza.image.asset.fluid}/>
+        <div>
+            <h2 className="mark">{pizza.name}</h2>
+            <ul>
+                {pizza.toppings.map(topping => <li key={topping.id}>
+                    {topping.name}
+                </li>)}
+            </ul>
+        </div>
+    </div>
 }
 
 //this needs to be dynamic based on the slug passed in via context in gatsby-node.js
