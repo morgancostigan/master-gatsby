a839b2806e1b732cf3a99c8494569670dbe8f3d1
morgancostigan <morgan.costigan@gmail.com>
2021-01-22 13:51:00 -0600

    beer data being queried on beers.js page

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,29 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
+    console.log({data});
+    
     return (
         <>
             <p>Kia Ora, I'm the beer page!</p>
         </>
     );
 }
+
+export const query = graphql`
+    query {
+        allBeer{
+            nodes{
+                id
+                name
+                price
+                image 
+                rating{
+                    reviews
+                    average
+                }
+            }
+        }
+    }
+`;
