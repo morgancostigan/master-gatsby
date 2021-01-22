54af1622a76709061021894153d25c9dcb8c2c3c
morgancostigan <morgan.costigan@gmail.com>
2021-01-22 16:06:03 -0600

    added styling to slicemasters and moved image to within the Link

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,6 +1,30 @@
 import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const SlicemastersGridStyles = styled.div`
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SingleSlicemasterStyles = styled.div`
+    a {
+        text-decoration: none;
+    }
+    .gatsby-image-wrapper {
+        height: 400px;
+    }
+    h2 {
+        transform: rotate(-12deg);
+        text-align: center;
+        font-size: 4rem;
+        margin-bottom: -2rem;
+        position: relative;
+        z-index: 2;
+    }
+`;
 
 export default function SliceMastersPage({ data }) {
     const slicemasters = data.people.nodes;
@@ -9,19 +33,20 @@ export default function SliceMastersPage({ data }) {
     return (
         <>
             <p>Kia Ora, I'm the slicemasters page!</p>
-            <div>
+            <SlicemastersGridStyles>
                 {slicemasters.map( person => (
-                    <div>
+                    <SingleSlicemasterStyles>
                         <Link to={`/slicemaster/${person.slug.current}`}>
                             <h2>
                                 <span className="mark">{person.name}</span>
                             </h2>
+                            <Img fluid={person.image.asset.fluid} alt={person.name}></Img>
+
                         </Link>
-                        <Img fluid={person.image.asset.fluid} alt={person.name}></Img>
                         <p className="description">{person.description}</p>
-                    </div>
+                    </SingleSlicemasterStyles>
                 ))}
-            </div>
+            </SlicemastersGridStyles>
         </ >
     );
 }
