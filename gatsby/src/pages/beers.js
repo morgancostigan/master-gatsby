1ebce87e260de1b5cea7efce7ac285bb65a7b1cc
morgancostigan <morgan.costigan@gmail.com>
2021-01-22 14:15:20 -0600

    beer data now all displaying on the dom

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -6,7 +6,28 @@ export default function BeersPage({ data }) {
     
     return (
         <>
-            <p>Kia Ora, I'm the beer page!</p>
+            <h2 className="center">We have {data.beers.nodes.length} beers available!</h2>
+            <h3 className="center">Dine in only.</h3>
+            <div>
+                {data.beers.nodes.map(beer => {
+                    const rating = Math.round(beer.rating.average);
+                    return (
+                        <div key={beer.id}>
+                            <img src={beer.image} alt={beer.name}></img>
+                            <h3>{beer.name}</h3>
+                            {beer.price}
+                            <p title={`${rating} out of 5 stars, with ${beer.rating.reviews} reviews.`}>
+                                {`⭐`.repeat(rating)}
+                                <span style={{ filter: `grayscale(90%)`}}>
+                                    {`⭐`.repeat(5 - rating)}
+                                </span>
+                                <span>({beer.rating.reviews} reviews.)</span>
+                            </p>
+
+                        </div>
+                    )
+                })}
+            </div>
         </>
     );
 }
