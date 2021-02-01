ef095330c388ce44fe36451e19d02464b341ae6a
morgancostigan <morgan.costigan@gmail.com>
2021-02-01 16:29:41 -0600

    added title to SEO in pizzas.js with an if statement determining whether a specific topping is highlighted in pageContext

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,6 +1,7 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 import PizzaList from '../components/PizzaList';
+import SEO from '../components/SEO';
 import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({data, pageContext}) {
@@ -8,6 +9,7 @@ export default function PizzasPage({data, pageContext}) {
     
     return (
         <>
+            <SEO title={pageContext.topping ? `Pizzas with ${pageContext.topping}` : `All Pizzas`}/>
             <ToppingsFilter activeTopping={pageContext.topping}/>
             {/* <p>Kia Ora, I'm the pizza page! There are {pizzas.length} pizzas.</p> */}
             <PizzaList pizzas={pizzas}/>
