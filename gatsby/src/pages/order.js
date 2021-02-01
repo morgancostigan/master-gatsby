21c648ee676a37789d13fa0d06f20fbd4f6b67ad
morgancostigan <morgan.costigan@gmail.com>
2021-02-01 16:36:11 -0600

    imported SEO into order.js and added title

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 export default function OrderPage() {
     return (
         <>
+            <SEO title="Order Page"/>
             <p>Kia Ora, I'm the order page!</p>
         </>
     );
