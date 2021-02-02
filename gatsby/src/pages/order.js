88a136d7e7bb2f748fcc76976f72a6f0d40da305
morgancostigan <morgan.costigan@gmail.com>
2021-02-02 12:59:10 -0600

    began skeleton of form on oder.js page

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -6,6 +6,17 @@ export default function OrderPage() {
         <>
             <SEO title="Order Page"/>
             <p>Kia Ora, I'm the order page!</p>
+            <form>
+                <fieldset>
+                    <legend>Your Info</legend>
+                </fieldset>
+                <fieldset>
+                    <legend>Menu</legend>
+                </fieldset>
+                <fieldset>
+                    <legend>Order</legend>
+                </fieldset>
+            </form>
         </>
     );
 }
