97395fee421f431544c076e5a0f22c5861dc8faf
morgancostigan <morgan.costigan@gmail.com>
2021-02-03 15:32:44 -0600

    created PizzaOrder.js and imported it into order.js

---
+++ gatsby/src/components/PizzaOrder.js
@@ -0,0 +1,15 @@
+ import React from 'react';
+import usePizza from '../utils/usePizza';
+
+ export default function PizzaOrder({ 
+     order, 
+     pizzas,
+     removeFromOrder,
+     plainImage
+ }) {
+    return (
+        <>
+            <p>Order!!!!!!!</p>
+        </>
+    )
+ }
