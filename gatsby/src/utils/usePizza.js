8bf5cdd56d702ae784e3a56c70554433957fc464
morgancostigan <morgan.costigan@gmail.com>
2021-02-03 11:17:23 -0600

    created usePizza.js util that will eventually be used to modify, calculate and send orders

---
+++ gatsby/src/utils/usePizza.js
@@ -0,0 +1,6 @@
+export default function usePizza({ pizzas, inputs }) {
+    //1. create state that holds order
+    //2. make an add to order function
+    //3. make a remove function
+    //4. send data to a serverless func on checkout
+}
