5eb578741d699b5ba96276d32a0839ef5a7cd739
morgancostigan <morgan.costigan@gmail.com>
2021-02-03 11:25:51 -0600

    built out most of usePizza.js

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 export default function usePizza({ pizzas, inputs }) {
     //1. create state that holds order
+    const [order, setOrder] = useState([]);
     //2. make an add to order function
+    function addToOrder(orderedPizza) {
+        setOrder([...order, orderedPizza ]);
+    };
     //3. make a remove function
+    function removeFromOrder(index) {
+        setOrder([
+            //everything before this item we are removing
+            ...order.slice(0, index),
+            //everything after this item we are removing
+            ...order.slice(index + 1) 
+        ]);
+    }
     //4. send data to a serverless func on checkout
+    //TODO
+    return {
+        order,
+        addToOrder,
+        removeFromOrder,
+    }
 }
