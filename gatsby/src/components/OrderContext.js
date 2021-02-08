5a52bf7a6ef86e9f950252ffe8896ab751d72a36
morgancostigan <morgan.costigan@gmail.com>
2021-02-08 11:30:28 -0600

    built OrderContext.js component to persist state at gatsby root level

---
+++ gatsby/src/components/OrderContext.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+//create an order context
+const OrderContext = React.createContext();
+
+//create a provider
+export function OrderProvider({ children }){
+    //put state in here
+    const [order, setOrder] = useState('🐩' );
+    return <OrderContext.Provider>
+        {children}
+    </OrderContext.Provider>
+}
+
+export default OrderContext;
+ 
