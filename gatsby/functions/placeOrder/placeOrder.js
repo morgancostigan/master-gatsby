44773b8ad3bb88cbffbf898654ccf564db2e4dc0
morgancostigan <morgan.costigan@gmail.com>
2021-02-09 13:14:10 -0600

    placeOrder now sends an email through a dummy email account set up at ethereal.email

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -9,3 +9,21 @@ const transporter = nodemailer.createTransport({
         pass: process.env.MAIL_PASS
     }
 });
+
+
+
+exports.handler = async(event, context) => {
+    //test send email
+    const info = await transporter.sendMail({
+        from: "Slick's Slices <slick@example.com>",
+        to: "orders@example.com",
+        subject: "New Order!",
+        html: `<p>You've got a new order!</p>`
+    })
+    console.log({info});
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify(info)
+    }
+}
