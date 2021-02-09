e3b255aaae1b469449915e0a04d7f8a2e5e55366
morgancostigan <morgan.costigan@gmail.com>
2021-02-09 13:01:16 -0600

    in placeOrder.js, required nodemailer, then cretaed transporter with .env variables

---
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+//create a transport for nodemailer
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+});
