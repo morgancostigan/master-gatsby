5d07dc81aac635bf0b32b0ebf4437ca9e1883930
morgancostigan <morgan.costigan@gmail.com>
2021-02-15 13:40:17 -0600

    continued building out sidebar.js

--- sanity/sidebar.js
+++ sanity/sidebar.js
@@ -1,12 +1,21 @@
 import React, { Children } from 'react';
-import S from '@sanity/desk-tool/structure-builder';
+import S from "@sanity/desk-tool/structure-builder";
+import { DocumentBuilder } from '@sanity/structure/lib/Document';
 
 //build a custom sidebar
 
- export default function Sidebar() {
-     return S.list().title(`Slick's Slices`).items([
-         //create a new item
-         S.listItem().title(`Home Page`).icon(() => 
-             <strong>🏠 </strong>)
-     ])
- };
+export default function Sidebar() {
+    return S.list().title(`Slick's Slices`).items([
+        //create a new item
+        S.listItem().title(`Home Page`).icon(() => 
+            <strong>🏠 </strong>)
+            .child(
+                S.editor()
+                .schemaType(`Store Settings`)
+                //make a new document ID, instead of random string
+                .documentId(`downtown`)
+            ),
+            //add in the rest of our document items
+        ...S.documentTypeListItems(),
+    ]);
+};
